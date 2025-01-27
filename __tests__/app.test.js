dabc0d38092541f73787e852607a5e268c95ec6d
aldaldaldald <183981294+aldaldaldald@users.noreply.github.com>
2025-01-27 14:19:49 +0000

    Set up the test suite and build an endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,9 @@
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
 
+const request = require("supertest");
+const app = require("../app.js");
+
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
